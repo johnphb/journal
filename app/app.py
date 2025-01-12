920cc4e63594e7d68340d26b976f2f62358be2f8
johnphb <john.berger@code.berlin>
2025-01-12 22:00:01 +0100

    deleted migrations

--- app/app.py
+++ app/app.py
@@ -28,6 +28,9 @@ def create_app():
     register_extensions(app)
     register_blueprints(app)
 
+    with app.app_context():
+        db.create_all()
+
     return app
 
 def register_blueprints(app: Flask):
@@ -47,4 +50,4 @@ def register_extensions(app: Flask):
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-    
+
