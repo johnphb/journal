9e0a3c283dc06f297af4d5937f1d730c54a3be91
johnphb <john.berger@code.berlin>
2025-01-12 21:32:18 +0100

    migrations

---
+++ migrations/versions/e8076749573c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e8076749573c
+Revises: e8076749573c
+Create Date: 2025-01-12 09:10:04.662968
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e8076749573c'
+down_revision = 'e8076749573c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
